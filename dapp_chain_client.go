31f52b3a1e2ab5c21f21b4d21c9a92d4e49c45cb
Vadim Macagon <vadim.macagon@gmail.com>
2018-04-15 20:10:45 +0700

    Add methods to DAppChainClient interface for committing call/deploy txs

--- dapp_chain_client.go
+++ dapp_chain_client.go
@@ -1,5 +1,14 @@
 package loomplugin
 
+type VMType int32
+
+const (
+	VMType_PLUGIN VMType = 0
+	VMType_EVM    VMType = 1
+)
+
 type DAppChainClient interface {
-	CommitTx(signer Signer, txBytes []byte) error
+	CommitTx(signer Signer, txBytes []byte) ([]byte, error)
+	CommitDeployTx(from Address, signer Signer, vm VMType, code []byte) ([]byte, error)
+	CommitCallTx(from Address, to Address, signer Signer, input []byte) ([]byte, error)
 }
