7b62522ac82b5508f470174d8c1e99c5cac02558
sriep <piers.shepperson@btinternet.com>
2018-05-14 18:11:57 +0100

    Add staticCall and staticCallEvm

--- plugin/contract.go
+++ plugin/contract.go
@@ -27,6 +27,7 @@ type Message struct {
 
 type StaticAPI interface {
 	StaticCall(addr loom.Address, input []byte) ([]byte, error)
+	StaticCallEVM(addr loom.Address, input []byte) ([]byte, error)
 	Resolve(name string) (loom.Address, error)
 	ValidatorPower(pubKey []byte) int64
 	Emit(event []byte)
