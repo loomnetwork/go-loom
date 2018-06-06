537552a9e9a0158ac2f66c3d941b8c35a3313fd6
Ady Romantika <ady@loomx.io>
2018-06-06 16:13:20 +0800

    Trigger downstream build on tiles-chain

---
+++ .travis_trigger_build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+if [ "${TRAVIS_BRANCH}" != "master" ]; then
+  echo "Not triggering downstream build since this is not master"
+  exit 0
+fi
+
+body='{
+"request": {
+"message": "Triggered by go-loom",
+"branch": "master"
+}}'
+
+curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token ${TRAVIS_TOKEN}" \
+  -d "$body" \
+  https://api.travis-ci.org/repo/loomnetwork/tiles-chain/requests
