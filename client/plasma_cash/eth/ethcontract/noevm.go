7786273d5b2a950b6b4749a8b276d60ca82ab546
Matthew Campbell <hyper@hyperworks.nu>
2018-07-04 00:39:30 +0700

    fix non evm builds

---
+++ client/plasma_cash/eth/ethcontract/noevm.go
@@ -0,0 +1,5 @@
+// !build evm
+
+package ethcontract
+
+//Dummy file that stubs out this package if we dont have the evm
---
+++ client/plasma_cash/eth/noevm.go
@@ -0,0 +1,5 @@
+// !build evm
+
+package eth
+
+//Dummy file that stubs out this package if we dont have the evm
