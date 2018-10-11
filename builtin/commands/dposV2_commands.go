d9223aec727496f896501758fc6eb5028a5310c0
Roy Blankman <riblankman@gmail.com>
2018-10-11 10:47:46 -0400

    Adding types and commands required for token delegation in DPOS
    firsts steps to supporting delegation types
    
    lowercasing package name, fixing delegationlist typo
    
    updating file naming
    
    updating dposv2 field names
    
    fixing amount fields...should have always been biguints
    
    making delegation store BigUint for delegation amount
    
    adding delegate and unbond commands to dposV2_commands

--- builtin/commands/dposV2_commands.go
+++ builtin/commands/dposV2_commands.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/loomnetwork/go-loom/builtin/types/dposv2"
 	"github.com/loomnetwork/go-loom/cli"
+	"github.com/loomnetwork/go-loom/types"
 )
 
 const DPOSV2ContractName = "dposV2"
@@ -17,8 +18,8 @@ func ListValidatorsCmdV2() *cobra.Command {
 		Use:   "list_validatorsV2",
 		Short: "List the current validators",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			var resp dpos.ListValidatorsResponseV2
-			err := cli.StaticCallContract(DPOSV2ContractName, "ListValidators", &dpos.ListValidatorsRequestV2{}, &resp)
+			var resp dposv2.ListValidatorsResponseV2
+			err := cli.StaticCallContract(DPOSV2ContractName, "ListValidators", &dposv2.ListValidatorsRequestV2{}, &resp)
 			if err != nil {
 				return err
 			}
@@ -37,8 +38,8 @@ func ListCandidatesCmdV2() *cobra.Command {
 		Use:   "list_candidatesV2",
 		Short: "List the registered candidates",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			var resp dpos.ListCandidateResponseV2
-			err := cli.StaticCallContract(DPOSV2ContractName, "ListCandidates", &dpos.ListCandidateRequestV2{}, &resp)
+			var resp dposv2.ListCandidateResponseV2
+			err := cli.StaticCallContract(DPOSV2ContractName, "ListCandidates", &dposv2.ListCandidateRequestV2{}, &resp)
 			if err != nil {
 				return err
 			}
@@ -62,7 +63,7 @@ func RegisterCandidateCmdV2() *cobra.Command {
 			if err != nil {
 				return err
 			}
-			return cli.CallContract(DPOSV2ContractName, "RegisterCandidate", &dpos.RegisterCandidateRequestV2{
+			return cli.CallContract(DPOSV2ContractName, "RegisterCandidate", &dposv2.RegisterCandidateRequestV2{
 				PubKey: pubKey,
 			}, nil)
 		},
@@ -85,7 +86,7 @@ func VoteCmdV2() *cobra.Command {
 				return err
 			}
 
-			return cli.CallContract(DPOSV2ContractName, "Vote", &dpos.VoteRequestV2{
+			return cli.CallContract(DPOSV2ContractName, "Vote", &dposv2.VoteRequestV2{
 				CandidateAddress: addr.MarshalPB(),
 				Amount:           amount,
 			}, nil)
@@ -93,12 +94,63 @@ func VoteCmdV2() *cobra.Command {
 	}
 }
 
+func DelegateCmdV2() *cobra.Command {
+	return &cobra.Command{
+		Use:   "delegateV2 [validator address] [amount]",
+		Short: "delegate tokens to a validator",
+		Args:  cobra.MinimumNArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			addr, err := cli.ResolveAddress(args[0])
+			if err != nil {
+				return err
+			}
+			amount, err := cli.ParseAmount(args[1])
+			if err != nil {
+				return err
+			}
+
+			return cli.CallContract(DPOSV2ContractName, "Delegate", &dposv2.DelegateRequestV2{
+				ValidatorAddress: addr.MarshalPB(),
+				Amount:           &types.BigUInt{
+					Value: *amount,
+				},
+			}, nil)
+		},
+	}
+}
+
+func UnbondCmdV2() *cobra.Command {
+	return &cobra.Command{
+		Use:   "unbondV2 [validator address] [amount]",
+		Short: "De-allocate tokens from a validator",
+		Args:  cobra.MinimumNArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			addr, err := cli.ResolveAddress(args[0])
+			if err != nil {
+				return err
+			}
+
+			amount, err := cli.ParseAmount(args[1])
+			if err != nil {
+				return err
+			}
+			return cli.CallContract(DPOSV2ContractName, "Unbond", &dposv2.UnbondRequestV2{
+				ValidatorAddress: addr.MarshalPB(),
+				Amount:           &types.BigUInt{
+					Value: *amount,
+				},
+			}, nil)
+		},
+	}
+}
+
+
 func ElectCmdV2() *cobra.Command {
 	return &cobra.Command{
 		Use:   "electV2",
 		Short: "Run an election",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return cli.CallContract(DPOSV2ContractName, "Elect", &dpos.ElectRequestV2{}, nil)
+			return cli.CallContract(DPOSV2ContractName, "Elect", &dposv2.ElectRequestV2{}, nil)
 		},
 	}
 }
@@ -110,5 +162,7 @@ func AddDPOSV2(root *cobra.Command) {
 		VoteCmdV2(),
 		ElectCmdV2(),
 		ListCandidatesCmdV2(),
+		DelegateCmdV2(),
+		UnbondCmdV2(),
 	)
 }
