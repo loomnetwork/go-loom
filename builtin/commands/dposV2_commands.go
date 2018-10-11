50397c98fa290021b8713fe98075ba485b74e302
Roy Blankman <riblankman@gmail.com>
2018-10-10 21:07:15 -0400

    more fixes

--- builtin/commands/dposV2_commands.go
+++ builtin/commands/dposV2_commands.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/spf13/cobra"
 
-	"github.com/loomnetwork/go-loom/builtin/types/dposV2"
+	"github.com/loomnetwork/go-loom/builtin/types/dposv2"
 	"github.com/loomnetwork/go-loom/cli"
 )
 
