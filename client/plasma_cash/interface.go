eae5bb02c9e859457ce1ae69b303396ef0e7780f
Parth <desaiparth08@gmail.com>
2018-11-01 17:12:56 +0530

    Changes to client

--- client/plasma_cash/interface.go
+++ client/plasma_cash/interface.go
@@ -21,6 +21,7 @@ type Block interface {
 type Tx interface {
 	RlpEncode() ([]byte, error)
 	Sign(key *ecdsa.PrivateKey) ([]byte, error)
+	Hash() ([]byte, error)
 	Sig() []byte
 	NewOwner() common.Address
 	Proof() Proof
@@ -39,7 +40,7 @@ type ChainServiceClient interface {
 	SubmitBlock() error
 	Deposit(deposit *pctypes.DepositRequest) error
 
-	SendTransaction(slot uint64, prevBlock *big.Int, denomination *big.Int, newOwner string, prevOwner string, sig []byte) error
+	SendTransaction(slot uint64, prevBlock *big.Int, denomination *big.Int, newOwner string, prevOwner string, sig []byte, hash []byte) error
 }
 
 type Account struct {
--- client/plasma_cash/transaction.go
+++ client/plasma_cash/transaction.go
@@ -19,6 +19,7 @@ type LoomTx struct {
 	Denomination *big.Int
 	Owner        common.Address
 	PrevBlock    *big.Int
+	Nonce        uint64
 	Signature    []byte
 	TXProof      []byte
 }
@@ -36,7 +37,12 @@ func (l *LoomTx) NewOwner() common.Address {
 }
 
 func (l *LoomTx) Sign(key *ecdsa.PrivateKey) ([]byte, error) {
-	sig, err := evmcompat.SoliditySign(l.Hash(), key)
+	hash, err := l.Hash()
+	if err != nil {
+		return nil, err
+	}
+
+	sig, err := evmcompat.SoliditySign(hash, key)
 	if err != nil {
 		return nil, err
 	}
@@ -52,26 +58,27 @@ func (l *LoomTx) RlpEncode() ([]byte, error) {
 		l.PrevBlock,
 		l.Denomination,
 		l.Owner,
+		l.Nonce,
 	})
 }
 
-func (l *LoomTx) Hash() []byte {
+func (l *LoomTx) Hash() ([]byte, error) {
 	if l.PrevBlock.Cmp(big.NewInt(0)) != 0 {
 		ret, err := l.rlpEncodeWithSha3()
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
-		return ret
+		return ret, nil
 	}
 
 	data, err := soliditySha3(l.Slot)
 	if err != nil {
-		panic(err) //TODO cleanup error interface
+		return nil, err
 	}
 	if len(data) != 32 {
-		panic(fmt.Sprintf("wrong hash size! expected 32, got %v", len(data)))
+		return nil, fmt.Errorf("wrong hash size! expected 32, got %v", len(data))
 	}
-	return data
+	return data, nil
 }
 
 func (l *LoomTx) MerkleHash() []byte {
