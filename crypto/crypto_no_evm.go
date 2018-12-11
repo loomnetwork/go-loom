a01eb080ae606ca8ddbb2a86afad33727a7d0137
Matthew Campbell <hyper@hyperworks.nu>
2018-12-11 20:12:30 +0700

    make compiles work on non evm builds for hsm

--- crypto/crypto_no_evm.go
+++ crypto/crypto_no_evm.go
@@ -5,3 +5,11 @@ package crypto
 func Sign(hash []byte, prv PrivateKey) (sig []byte, err error) {
 	panic("EVM build isn't activated")
 }
+
+func LoadSecp256k1PrivKey(filePath string) (interface{}, error) {
+	panic("EVM build isn't activated")
+}
+
+func LoadYubiHsmPrivKey(filePath string) (interface{}, error) {
+	panic("EVM build isn't activated")
+}
