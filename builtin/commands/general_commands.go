5a48d50db1be1c5d421ec48cd9fe3875ddc99a63
Georgios Konstantopoulos <me@gakonst.com>
2018-12-20 16:47:40 +0200

    Add resolve pubkey command

---
+++ builtin/commands/general_commands.go
@@ -0,0 +1,30 @@
+package commands
+
+import (
+	"encoding/base64"
+	"fmt"
+
+	loom "github.com/loomnetwork/go-loom"
+	"github.com/spf13/cobra"
+)
+
+func PubkeyToAddress() *cobra.Command {
+	return &cobra.Command{
+		Use:   "pubkey [public key]",
+		Short: "Converts a public key to an address",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			pubKey, err := base64.StdEncoding.DecodeString(args[0])
+			if err != nil {
+				return err
+			}
+			fmt.Println("Address", loom.LocalAddressFromPublicKey(pubKey))
+			return nil
+		},
+	}
+}
+
+func AddGeneralCommands(root *cobra.Command) {
+	root.AddCommand(
+		PubkeyToAddress(),
+	)
+}
