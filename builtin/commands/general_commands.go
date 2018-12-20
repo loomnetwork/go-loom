dc1b1495e037127c1f344e2441717d5c0f6e1049
Georgios Konstantopoulos <me@gakonst.com>
2018-12-20 16:52:24 +0200

    add min commands

--- builtin/commands/general_commands.go
+++ builtin/commands/general_commands.go
@@ -12,6 +12,7 @@ func PubkeyToAddress() *cobra.Command {
 	return &cobra.Command{
 		Use:   "pubkey [public key]",
 		Short: "Converts a public key to an address",
+		Args:  cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			pubKey, err := base64.StdEncoding.DecodeString(args[0])
 			if err != nil {
