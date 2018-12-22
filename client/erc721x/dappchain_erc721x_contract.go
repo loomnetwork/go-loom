24db2aa70f61acbeef26b92ddbf12871a683ffd0
Georgios Konstantopoulos <me@gakonst.com>
2018-12-22 21:04:16 +0200

    Add ERC721X

---
+++ client/erc721x/dappchain_erc721x_contract.go
@@ -0,0 +1,67 @@
+package erc721x
+
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/loomnetwork/go-loom"
+	"github.com/loomnetwork/go-loom/auth"
+	"github.com/loomnetwork/go-loom/client"
+)
+
+type DAppChainERC721XContract struct {
+	*client.MirroredTokenContract
+}
+
+func (c *DAppChainERC721XContract) OwnerOf(tokenID *big.Int) (loom.LocalAddress, error) {
+	var result common.Address
+	if err := c.StaticCallEVM("ownerOf", &result, tokenID); err != nil {
+		return nil, err
+	}
+	addr, err := loom.LocalAddressFromHexString(result.Hex())
+	if err != nil {
+		return nil, err
+	}
+	return addr, nil
+}
+
+// SetApprovalForAll grants or revokes authorization to an entity to transfer any tokens from the
+// owner account at a later time.
+func (c *DAppChainERC721XContract) SetApprovalForAll(owner *client.Identity, operator loom.Address, isApproved bool) error {
+	operatorAddr := common.BytesToAddress(operator.Local)
+	return c.CallEVM("setApprovalForAll", owner.LoomSigner, operatorAddr, isApproved)
+}
+
+func (c *DAppChainERC721XContract) BalanceOf(identity *client.Identity, tokenID *big.Int) (*big.Int, error) {
+	result := new(big.Int)
+	addr := common.BytesToAddress(identity.LoomAddr.Local)
+	if err := c.StaticCallEVM("balanceOfToken", &result, addr, tokenID); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (c *DAppChainERC721XContract) TransferFrom(from *client.Identity, to *client.Identity, tokenID, amount *big.Int) error {
+	fromAddr := common.BytesToAddress(from.LoomAddr.Local)
+	toAddr := common.BytesToAddress(to.LoomAddr.Local)
+	return c.CallEVM("transferFrom", from.LoomSigner, fromAddr, toAddr, tokenID, amount)
+}
+
+func DeployERC721XToDAppChain(loomClient *client.DAppChainRPCClient, contractName string,
+	gatewayAddr loom.Address, creator auth.Signer) (*DAppChainERC721XContract, error) {
+	contract, err := client.DeployTokenToDAppChain(loomClient, contractName, "erc721x", gatewayAddr, creator)
+	if err != nil {
+		return nil, err
+	}
+	return &DAppChainERC721XContract{contract}, nil
+}
+
+func ConnectERC721XToDAppChain(
+	loomClient *client.DAppChainRPCClient, contractName string,
+) (*DAppChainERC721XContract, error) {
+	contract, err := client.ConnectToMirroredTokenContract(loomClient, contractName, "erc721x")
+	if err != nil {
+		return nil, err
+	}
+	return &DAppChainERC721XContract{contract}, nil
+}
---
+++ client/erc721x/mainnet_erc721x_contract.go
@@ -0,0 +1,60 @@
+package erc721x
+
+import (
+	"context"
+	"math/big"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/loomnetwork/go-loom/client"
+)
+
+type MainnetERC721XClient struct {
+	contract  *ERC721X
+	ethClient *ethclient.Client
+
+	TxTimeout time.Duration
+	Address   common.Address
+}
+
+func (c *MainnetERC721XClient) BalanceOf(owner common.Address) (*big.Int, error) {
+	bal, err := c.contract.BalanceOf(nil, owner)
+	if err != nil {
+		return nil, err
+	}
+	return bal, nil
+}
+
+func (c *MainnetERC721XClient) TokenOfOwnerByIndex(owner common.Address, index int) (*big.Int, error) {
+	tokenID, err := c.contract.TokenOfOwnerByIndex(nil, owner, new(big.Int).SetInt64(int64(index)))
+	if err != nil {
+		return nil, err
+	}
+	return tokenID, nil
+}
+
+func (c *MainnetERC721XClient) OwnerOf(tokenID *big.Int) (common.Address, error) {
+	return c.contract.OwnerOf(nil, tokenID)
+}
+
+func (c *MainnetERC721XClient) SafeTransferFrom(caller *client.Identity, from common.Address, to common.Address, tokenId *big.Int, amount *big.Int, data []byte) error {
+	tx, err := c.contract.SafeTransferFrom(client.DefaultTransactOptsForIdentity(caller), from, to, tokenId, amount, data)
+	if err != nil {
+		return err
+	}
+	return client.WaitForTxConfirmation(context.TODO(), c.ethClient, tx, c.TxTimeout)
+}
+
+func ConnectToMainnetERC721XClient(ethClient *ethclient.Client, contractAddr string) (*MainnetERC721XClient, error) {
+	contractAddress := common.HexToAddress(contractAddr)
+	contract, err := NewERC721X(contractAddress, ethClient)
+	if err != nil {
+		return nil, err
+	}
+	return &MainnetERC721XClient{
+		contract:  contract,
+		ethClient: ethClient,
+		Address:   contractAddress,
+	}, nil
+}
