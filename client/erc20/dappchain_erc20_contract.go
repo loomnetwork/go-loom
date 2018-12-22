02a7af2538a2042787a463bb0b793786eae3bb19
Georgios Konstantopoulos <me@gakonst.com>
2018-12-22 21:04:02 +0200

    Add ERC20

---
+++ client/erc20/dappchain_erc20_contract.go
@@ -0,0 +1,52 @@
+package erc20
+
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/loomnetwork/go-loom"
+	"github.com/loomnetwork/go-loom/auth"
+	"github.com/loomnetwork/go-loom/client"
+)
+
+type DAppChainERC20Contract struct {
+	*client.MirroredTokenContract
+}
+
+func (c *DAppChainERC20Contract) BalanceOf(caller *client.Identity) (*big.Int, error) {
+	ownerAddr := common.BytesToAddress(caller.LoomAddr.Local)
+	var result *big.Int
+	if err := c.StaticCallEVM("balanceOf", &result, ownerAddr); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+// Approve grants authorization to an entity to transfer the given tokens at a later time
+func (c *DAppChainERC20Contract) Approve(identity *client.Identity, to loom.Address, amount *big.Int) error {
+	toAddr := common.BytesToAddress(to.Local)
+	return c.CallEVM("approve", identity.LoomSigner, toAddr, amount)
+}
+
+/**
+  Functions to connect / deploy the ERC20 contracts
+*/
+
+func DeployERC20ToDAppChain(loomClient *client.DAppChainRPCClient, contractName string,
+	gatewayAddr loom.Address, creator auth.Signer) (*DAppChainERC20Contract, error) {
+	contract, err := client.DeployTokenToDAppChain(loomClient, contractName, "erc20", gatewayAddr, creator)
+	if err != nil {
+		return nil, err
+	}
+	return &DAppChainERC20Contract{contract}, nil
+}
+
+func ConnectERC20ToDAppChain(
+	loomClient *client.DAppChainRPCClient, contractName string,
+) (*DAppChainERC20Contract, error) {
+	contract, err := client.ConnectToMirroredTokenContract(loomClient, contractName, "erc20")
+	if err != nil {
+		return nil, err
+	}
+	return &DAppChainERC20Contract{contract}, nil
+}
---
+++ client/erc20/mainnet_erc20_client.go
@@ -0,0 +1,64 @@
+package erc20
+
+import (
+	"context"
+	"math/big"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/loomnetwork/go-loom/client"
+)
+
+type MainnetERC20Client struct {
+	contract  *ERC20
+	ethClient *ethclient.Client
+
+	TxTimeout time.Duration
+	Address   common.Address
+}
+
+func (c *MainnetERC20Client) BalanceOf(addr common.Address) (*big.Int, error) {
+	bal, err := c.contract.BalanceOf(nil, addr)
+	if err != nil {
+		return nil, err
+	}
+	return bal, nil
+}
+
+func (c *MainnetERC20Client) TransferFrom(caller *client.Identity, from common.Address, to common.Address, amount *big.Int) error {
+	tx, err := c.contract.TransferFrom(client.DefaultTransactOptsForIdentity(caller), from, to, amount)
+	if err != nil {
+		return err
+	}
+	return client.WaitForTxConfirmation(context.TODO(), c.ethClient, tx, c.TxTimeout)
+}
+
+func (c *MainnetERC20Client) Approve(caller *client.Identity, spender common.Address, amount *big.Int) error {
+	tx, err := c.contract.Approve(client.DefaultTransactOptsForIdentity(caller), spender, amount)
+	if err != nil {
+		return err
+	}
+	return client.WaitForTxConfirmation(context.TODO(), c.ethClient, tx, c.TxTimeout)
+}
+
+func (c *MainnetERC20Client) Transfer(caller *client.Identity, to common.Address, amount *big.Int) error {
+	tx, err := c.contract.Transfer(client.DefaultTransactOptsForIdentity(caller), to, amount)
+	if err != nil {
+		return err
+	}
+	return client.WaitForTxConfirmation(context.TODO(), c.ethClient, tx, c.TxTimeout)
+}
+
+func ConnectToMainnetERC20(ethClient *ethclient.Client, contractAddr string) (*MainnetERC20Client, error) {
+	contractAddress := common.HexToAddress(contractAddr)
+	contract, err := NewERC20(contractAddress, ethClient)
+	if err != nil {
+		return nil, err
+	}
+	return &MainnetERC20Client{
+		contract:  contract,
+		ethClient: ethClient,
+		Address:   contractAddress,
+	}, nil
+}
