1390ffee6b058a0f859344ed77fc4388b665e924
linuxdev53 <linux.dev53@gmail.com>
2019-01-21 10:20:24 +0200

    Added ed25519 key load function

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -1,8 +1,27 @@
 package crypto
 
+import (
+	"encoding/base64"
+	"io/ioutil"
+)
+
 const (
 	PrivateKeyTypeEd25519   = "ed25519"
 	PrivateKeyTypeSecp256k1 = "secp256k1"
 )
 
 type PrivateKey interface{}
+
+func LoadEd25519PrivKey(path string) ([]byte, error) {
+	privKeyB64, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	privKey, err := base64.StdEncoding.DecodeString(string(privKeyB64))
+	if err != nil {
+		return nil, err
+	}
+
+	return privKey, nil
+}
--- crypto/crypto_evm.go
+++ crypto/crypto_evm.go
@@ -3,10 +3,29 @@
 package crypto
 
 import (
+	"crypto/ecdsa"
 	"fmt"
 
 	"github.com/ethereum/go-ethereum/crypto"
 )
+
+type Secp256k1PrivateKey ecdsa.PrivateKey
+
+func (s *Secp256k1PrivateKey) ToECDSAPrivKey() *ecdsa.PrivateKey {
+	ecdsaPrivKey := ecdsa.PrivateKey(*s)
+	return &ecdsaPrivKey
+}
+
+func LoadSecp256k1PrivKey(filePath string) (*Secp256k1PrivateKey, error) {
+	privKey, err := crypto.LoadECDSA(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	secpPrivKey := Secp256k1PrivateKey(*privKey)
+
+	return &secpPrivKey, nil
+}
 
 func SoliditySign(hash []byte, prv PrivateKey) (sig []byte, err error) {
 	switch prv.(type) {
