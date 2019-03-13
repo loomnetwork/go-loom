1a80484e5b137f98fde0fc641d1f5b41effebe0e
Piers Shepperson <piers.shepperson@btinternet.com>
2019-03-13 13:15:07 +0000

    Support non evm build

--- auth/eth-signer-no-evm.go
+++ auth/eth-signer-no-evm.go
@@ -2,6 +2,18 @@
 
 package auth
 
-func NewEthSigner66Byte(privateKey []byte) Signer {
+type EthSigner66Byte struct {
+	PrivateKey interface{}
+}
+
+func NewEthSigner66Byte(_ []byte) Signer {
 	panic("EVM build isn't activated")
+}
+
+func (k *EthSigner66Byte) Sign(_ []byte) []byte {
+	return nil
+}
+
+func (k *EthSigner66Byte) PublicKey() []byte {
+	return nil
 }
