26bfc2c07a0d012c2e4416e75cf7d2e5e5155af5
Piers Shepperson <piers.shepperson@btinternet.com>
2019-03-13 14:48:23 +0000

    Remove unused code

--- auth/eth-signer.go
+++ auth/eth-signer.go
@@ -23,10 +23,6 @@ type EthSigner66Byte struct {
 	PrivateKey *ecdsa.PrivateKey
 }
 
-func NewEthSigner66Byte(_ []byte) Signer {
-	panic("EVM build isn't activated")
-}
-
 func (k *EthSigner66Byte) Sign(txBytes []byte) []byte {
 	var nonceTx NonceTx
 	if err := proto.Unmarshal(txBytes, &nonceTx); err != nil {
@@ -59,31 +55,4 @@ func (k *EthSigner66Byte) Sign(txBytes []byte) []byte {
 
 func (k *EthSigner66Byte) PublicKey() []byte {
 	return crypto.FromECDSAPub(&k.PrivateKey.PublicKey)
-}
-
-type EthSigner65Byte struct {
-	PrivateKey *ecdsa.PrivateKey
-}
-
-func NewEthSigner65Byte(_ []byte) Signer {
-	panic("EVM build isn't activated")
-}
-
-func (k *EthSigner65Byte) Sign(_ []byte) []byte {
-	sigBytes, err := evmcompat.SoliditySign(k.PublicKey(), k.PrivateKey)
-	if err != nil {
-		panic(err)
-	}
-	return sigBytes
-}
-
-func (k *EthSigner65Byte) PublicKey() []byte {
-	ethLocalAdr, err := loom.LocalAddressFromHexString(crypto.PubkeyToAddress(k.PrivateKey.PublicKey).Hex())
-	if err != nil {
-		panic(err)
-	}
-	ethPublicAddr := loom.Address{ChainID: EthChainId, Local: ethLocalAdr}
-	hash := crypto.Keccak256(ethPublicAddr.Bytes())
-
-	return hash
 }
