6455cba1e438abc8b11519204278fd9b4bb2062f
suhasagg <suhasagg@gmail.com>
2019-04-17 00:51:45 +0530

    Refactoring for commands migration to loomchain

--- cli/cli.go
+++ cli/cli.go
@@ -10,7 +10,19 @@ import (
 	"github.com/loomnetwork/go-loom/client"
 )
 
+type ContractCallFlags struct {
+	URI           string
+	MainnetURI    string
+	ContractAddr  string
+	ChainID       string
+	PrivFile      string
+	HsmConfigFile string
+	Algo          string
+	CallerChainID string
+}
+
 var TxFlags struct {
+	URI           string
 	WriteURI      string
 	ReadURI       string
 	MainnetURI    string
@@ -33,8 +45,8 @@ func ContractCallCommand(name string) *cobra.Command {
 			Short: "call a method of the " + name + " contract",
 		}
 	}
-
 	pflags := cmd.PersistentFlags()
+	pflags.StringVarP(&TxFlags.URI, "uri", "u", "http://localhost:46658", "DAppChain base URI")
 	pflags.StringVarP(&TxFlags.WriteURI, "write", "w", "http://localhost:46658/rpc", "URI for sending txs")
 	pflags.StringVarP(&TxFlags.ReadURI, "read", "r", "http://localhost:46658/query", "URI for quering app state")
 	pflags.StringVarP(&TxFlags.MainnetURI, "ethereum", "e", "http://localhost:8545", "URI for talking to Ethereum")
@@ -44,6 +56,7 @@ func ContractCallCommand(name string) *cobra.Command {
 	pflags.StringVarP(&TxFlags.HsmConfigFile, "hsmconfig", "", "", "hsm config file")
 	pflags.StringVar(&TxFlags.Algo, "algo", "ed25519", "Signing algo: ed25519, secp256k1, tron")
 	pflags.StringVar(&TxFlags.CallerChainID, "caller-chain", "", "Overrides chain ID of caller")
+
 	return cmd
 }
 
@@ -53,6 +66,7 @@ func ContractResolveCommand() *cobra.Command {
 		Short: "resolve a contract method",
 	}
 	pflags := cmd.PersistentFlags()
+	pflags.StringVarP(&TxFlags.URI, "uri", "u", "http://localhost:46658", "DAppChain base URI")
 	pflags.StringVarP(&TxFlags.WriteURI, "write", "w", "http://localhost:46658/rpc", "URI for sending txs")
 	pflags.StringVarP(&TxFlags.ReadURI, "read", "r", "http://localhost:46658/query", "URI for quering app state")
 	pflags.StringVarP(&TxFlags.MainnetURI, "ethereum", "e", "http://localhost:8545", "URI for talking to Ethereum")
@@ -63,6 +77,49 @@ func ContractResolveCommand() *cobra.Command {
 	pflags.StringVarP(&TxFlags.Algo, "algo", "", "ed25519", "crypto algo for the key- default is Ed25519 or Secp256k1")
 
 	return cmd
+}
+
+func contractv1(callFlags *ContractCallFlags, defaultAddr string) (*client.Contract, error) {
+	contractAddrStr := callFlags.ContractAddr
+	if contractAddrStr == "" {
+		contractAddrStr = defaultAddr
+	}
+
+	if contractAddrStr == "" {
+		return nil, errors.New("contract address or name required")
+	}
+
+	contractAddr, err := ResolveAddressv1(contractAddrStr, callFlags)
+	if err != nil {
+		return nil, err
+	}
+	// create rpc client
+	rpcClient := client.NewDAppChainRPCClient(callFlags.ChainID, callFlags.URI+"/rpc", callFlags.URI+"/query")
+	// create contract
+	contract := client.NewContract(rpcClient, contractAddr.Local)
+	return contract, nil
+}
+
+func CallContractv1(callFlags *ContractCallFlags, defaultAddr string, method string, params proto.Message, result interface{}) error {
+	signer, err := GetSigner(callFlags.PrivFile, callFlags.HsmConfigFile, callFlags.Algo)
+	if err != nil {
+		return err
+	}
+	contract, err := contractv1(callFlags, defaultAddr)
+	if err != nil {
+		return err
+	}
+	_, err = contract.Call(method, params, signer, result)
+	return err
+}
+
+func StaticCallContractv1(callFlags *ContractCallFlags, defaultAddr string, method string, params proto.Message, result interface{}) error {
+	contract, err := contractv1(callFlags, defaultAddr)
+	if err != nil {
+		return err
+	}
+	_, err = contract.StaticCall(method, params, loom.RootAddress(callFlags.ChainID), result)
+	return err
 }
 
 func contract(defaultAddr string) (*client.Contract, error) {
@@ -79,9 +136,8 @@ func contract(defaultAddr string) (*client.Contract, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	// create rpc client
-	rpcClient := client.NewDAppChainRPCClient(TxFlags.ChainID, TxFlags.WriteURI, TxFlags.ReadURI)
+	rpcClient := client.NewDAppChainRPCClient(TxFlags.ChainID, TxFlags.URI+"/rpc", TxFlags.URI+"/query")
 	// create contract
 	contract := client.NewContract(rpcClient, contractAddr.Local)
 	return contract, nil
