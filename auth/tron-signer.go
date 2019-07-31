6e2051a8c67b09bffa74b35ca29ae06954c351fb
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2019-07-31 16:35:50 +0700

    Fix signer to use the same signature function as tronweb

--- auth/tron-signer.go
+++ auth/tron-signer.go
@@ -33,7 +33,7 @@ func (k *TronSigner) Sign(txBytes []byte) []byte {
 	signature, err := evmcompat.GenerateTypedSig(
 		sha3.SoliditySHA3(
 			sha3.String("\x19TRON Signed Message:\n32"),
-			sha3.SoliditySHA3(txBytes),
+			txBytes,
 		),
 		k.PrivateKey,
 		evmcompat.SignatureType_TRON,
